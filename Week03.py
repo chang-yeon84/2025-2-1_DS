537025fc49ed87f767496b020c0736aaba1f7121
chang yeon <yeoun0852@gmail.com>
2025-03-24 18:06:56 +0900

    ver 1.0 array

--- Week03.py
+++ Week03.py
@@ -1,21 +1,11 @@
-def duplicates_city(an_iterable):
-    result_city = []
-    s = set()
+def inters(l1, l2):
+    l3 = []
+    for v in l1:
+        if v in l2:
+            l3.append(v)
 
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2:
-            result_city.append(city)
-    return result_city
+    return l3
 
-cities = ['Incheon','Seoul','Incheon','Incheon','Gwangju']
-# cities = set(cities)
-# cities.add('Incheon')
-# cities.add('Suwon')
-cities.append('Incheon')
-cities.append('Suwon')
-cities.append('Seoul')
-print(cities)
-print(set(duplicates_city(cities)))
+l1 = [45, 5 ,22 ,31, 7, 19]
+l2 = [22, 1, 5, 2, 7, 28, 27, 19, 13, 41]
+print(inters(l1, l2))
