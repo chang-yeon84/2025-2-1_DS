bd6f0e7eb2e3e18472453a552d88d69c90643f6c
chang yeon <yeoun0852@gmail.com>
2025-03-31 12:10:22 +0900

    v1.6 Linked List

--- Week04.py
+++ Week04.py
@@ -1,54 +1,63 @@
-import random
-
-
 class Node:
     def __init__(self, data, link=None):
         self.data = data
         self.link = link
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
 
+
     def append(self, data):
-        if not self.head: # 링크드 리스트 가 노드가 하나도 없는 상태면
-            self.head = Node(data) # 새 노드를 head 에 연결
+        if not self.head:
+            self.head = Node(data)
             return
         current = self.head
         while current.link:
-            current = current.link # 다음 노드 이동
-        current.next = Node(data)
+            current = current.link
+        current.link = Node(data)
 
+
+    def remove(self, target):
+        if self.head.data == target:
+            self.head = self.head.link
+            return
+        current = self.head
+        previous = None
+        while current:
+            if current.data == target:
+                previous.link = current.link
+            previous = current
+            current = current.link
 
 
     def search(self, target):
         current = self.head
         while current.link:
             if current.data == target:
-                return f"{target}"
+                return f"{target}을(를) 찾았습니다"
             else:
-                current = current.next
-            return f"{target} is not in LinkedList"
+                current = current.link
+        return f"{target}은(는) 링크드리스트 안에 존재하지 않습니다"
+
 
     def __str__(self):
         node = self.head
         out_texts = ""
         while node is not None:
-            out_texts = out_texts = f"{node.data} -> "
+            out_texts = out_texts + f"{node.data} -> "
             node = node.link
-        return out_texts + " end "
+        return out_texts + "end"
 
 
 ll = LinkedList()
 ll.append(8)
 ll.append(10)
 ll.append(-9)
-ll.search(10)
-ll.search(100)
-
-ll = LinkedList()
-for _ in range(0, 20):
-    ll.append(random.randint(1, 30))
-
 print(ll)
+print(ll.search(100))
 print(ll.search(10))
+ll.remove(90)
+ll.remove(10)
+print(ll)
