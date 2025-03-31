8d5bf41c3df07b4e690e13c874c9a4a8c9344799
chang yeon <yeoun0852@gmail.com>
2025-03-31 12:02:46 +0900

    v1.5 Linked List

--- Week04.py
+++ Week04.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Node:
     def __init__(self, data, link=None):
         self.data = data
@@ -16,6 +19,17 @@ class LinkedList:
             current = current.link # 다음 노드 이동
         current.next = Node(data)
 
+
+
+    def search(self, target):
+        current = self.head
+        while current.link:
+            if current.data == target:
+                return f"{target}"
+            else:
+                current = current.next
+            return f"{target} is not in LinkedList"
+
     def __str__(self):
         node = self.head
         out_texts = ""
@@ -24,7 +38,17 @@ class LinkedList:
             node = node.link
         return out_texts + " end "
 
+
 ll = LinkedList()
 ll.append(8)
 ll.append(10)
 ll.append(-9)
+ll.search(10)
+ll.search(100)
+
+ll = LinkedList()
+for _ in range(0, 20):
+    ll.append(random.randint(1, 30))
+
+print(ll)
+print(ll.search(10))
