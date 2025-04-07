31b49f6178c21b97f1da5ca19d71351604d02e5e
chang yeon <yeoun0852@gmail.com>
2025-04-07 11:47:53 +0900

    v2.3 stack

--- Week05.py
+++ Week05.py
@@ -1,31 +1,11 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.link = None
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.line = self.top
-            self.top = node
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty"
-        popped_node = self.top
-        self.top = self.top.link
-        return popped_node.data
-
-s1 = Stack()
+s1 = list()
+print(len(s1))
+s1.append("Data Structure")
+s1.append("DataBase")
+print(len(s1))
+print(s1[-1])
+print(s1)
 print(s1.pop())
-s1.push("자료구조")
-s1.push("데이터베이스")
-#print(s1.pop())
-#print(s1.pop())
-for i in range (3):
-    print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
